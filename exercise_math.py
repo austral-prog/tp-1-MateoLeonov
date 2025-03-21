35bc1ea0b518e1b1a7c6ec9fc18f4701efd96342
MateoLeonov <mjleonov@mail.austral.edu.ar>
2025-03-20 23:48:02 -0300

    ejercicio 1

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,11 @@
 def math():
     a = 57
-    b = 7
+    b = 7  #en a y b se ponen el numero entero
+    print(a + b)
+    print(a - b)
+    print(a * b)
+    print((a + b) / 2)
+    print(a // b)
+    print(a % b)
+    print(a / b)
+    
